327275cbc1ac1e451748021c554739f471cb0028
Aleksandergreg <Aleksandergreg@users.noreply.github.com>
2025-04-18 09:35:34 +0000

    deploy: 5bad583858ab475a120721dd5ea3c22614d64cb7

--- sinatra/tests/e2e/about.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('about page shows mission', async ({ page }) => {
-  await page.goto('/about');
-  await expect(page).toHaveTitle('DynaSearch 🧨');
-  await expect(page.getByRole('heading', { name: 'Our mission' })).toBeVisible();
-  await expect(page.locator('p')).toContainText("We intend to build the world's best search engine!");
-});
