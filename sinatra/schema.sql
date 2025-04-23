77c0b0acf86bc529d7d902db2027c6dbd0b15cac
Aleksandergreg <alek3691@stud.kea.dk>
2025-04-23 12:41:20 +0200

    Changed the SQL script to work with postgressql

--- sinatra/schema.sql
+++ sinatra/schema.sql
@@ -1,21 +1,28 @@
+-- Drop old table if it exists
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL UNIQUE,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
+-- Create users with a SERIAL primary key
+CREATE TABLE users (
+  id      SERIAL        PRIMARY KEY,
+  username TEXT         NOT NULL UNIQUE,
+  email    TEXT         NOT NULL UNIQUE,
+  password TEXT         NOT NULL
 );
 
--- Create a default user, The password is 'password' (MD5 hashed)
-INSERT INTO users (username, email, password) 
-    VALUES ('admin', 'keamonk1@stud.kea.dk', '5f4dcc3b5aa765d61d8327deb882cf99');
+-- Create an ENUM for language codes
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'language_enum') THEN
+    CREATE TYPE language_enum AS ENUM ('en', 'da');
+  END IF;
+END$$;
 
+DROP TABLE IF EXISTS pages;
 
-CREATE TABLE IF NOT EXISTS pages (
-    title TEXT PRIMARY KEY UNIQUE,
-    url TEXT NOT NULL UNIQUE,
-    language TEXT NOT NULL CHECK(language IN ('en', 'da')) DEFAULT 'en', -- How you define ENUM type in SQLite
-    last_updated TIMESTAMP,
-    content TEXT NOT NULL
+CREATE TABLE pages (
+  title         TEXT           PRIMARY KEY,
+  url           TEXT           NOT NULL UNIQUE,
+  language      language_enum  NOT NULL DEFAULT 'en',
+  last_updated  TIMESTAMP,
+  content       TEXT           NOT NULL
 );
